aaa2b99337e1d7cdac939edd2b3eeb320b69d8de
Morgan Funtowicz <funtowiczmo@gmail.com>
2015-12-27 13:40:44 +0100

    Message are displayed with the correct css style

--- spik-fx/src/main/java/com/polytech/spik/views/lists/MessageItem.java
+++ spik-fx/src/main/java/com/polytech/spik/views/lists/MessageItem.java
@@ -26,8 +26,6 @@ public class MessageItem extends ListCell<FXMessage> {
     private static final DateTimeFormatter TODAY_FORMATTER = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM);
     private static final DateTimeFormatter DEFAULT_FORMATTER = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
 
-    private static final String BUBBLE_PATH = "M25,4C12.318,4,2,12.972,2,24c0,6.187,3.334,12.065,8.951,15.839c-0.133,1.053-0.665,3.601-2.769,6.585l-1.105,1.567 L9.054,48c5.432,0,9.103-3.33,10.309-4.604C21.197,43.797,23.091,44,25,44c12.682,0,23-8.972,23-20S37.682,4,25,4z";
-
     private static final Pos SENT_MESSAGE = Pos.CENTER_LEFT;
     private static final Pos RECEIVED_MESSAGE = Pos.CENTER_RIGHT;
 
@@ -58,6 +56,9 @@ public class MessageItem extends ListCell<FXMessage> {
     protected void updateItem(FXMessage item, boolean empty) {
         super.updateItem(item, empty);
 
+        messageText.setVisible(!empty);
+        messageText.getStyleClass().clear();
+
         if (empty || item == null){
             messageText.setText(null);
             messageDate.setText(null);
@@ -65,7 +66,6 @@ public class MessageItem extends ListCell<FXMessage> {
             final LocalDateTime dateReceived = LocalDateTime.ofInstant(
                 Instant.ofEpochMilli(item.date()), ZoneId.systemDefault()
             );
-
 
             if(item.status() == Message.Status.SENT){
                 setAlignment(SENT_MESSAGE);
