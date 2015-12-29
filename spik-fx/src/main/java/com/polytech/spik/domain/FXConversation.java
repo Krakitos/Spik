1cdca1bef7b97ce452ed877b2037a328820ed638
Morgan Funtowicz <funtowiczmo@gmail.com>
2015-12-29 14:17:19 +0100

    Ability to retrieve last message date in the conversation

--- spik-fx/src/main/java/com/polytech/spik/domain/FXConversation.java
+++ spik-fx/src/main/java/com/polytech/spik/domain/FXConversation.java
@@ -79,6 +79,11 @@ public class FXConversation implements Conversation, Media {
 
     public SortedList<FXMessage> sortedMessagesProperty(){ return sortedMessages; }
 
+    @Override
+    public long lastMessageDate() {
+        return sortedMessages.get(sortedMessages.size() - 1).date();
+    }
+
     public String snippet() {
         return snippet.get();
     }
@@ -119,7 +124,7 @@ public class FXConversation implements Conversation, Media {
             Image toDisplay = participants.stream()
                     .filter(Contact::hasPicture)
                     .findAny()
-                    .map(c -> new Image(new ByteArrayInputStream(c.picture()), 64, 64, true, true))
+                    .map(c -> new Image( new ByteArrayInputStream(c.picture()), 32, 32, true, true))
                     .orElse(EMPTY_CONTACT);
 
             image = new SimpleObjectProperty<>(toDisplay);
